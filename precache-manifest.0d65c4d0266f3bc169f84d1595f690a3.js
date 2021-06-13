d25002101f7b0e5e1ad10cd3d05d2c42a42fbc77
Lem0n24 <lemon24eucf@gmail.com>
2021-06-13 17:12:08 +0700

    Updates

---
+++ precache-manifest.0d65c4d0266f3bc169f84d1595f690a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c062f4209a27600a5f5c2d5fbd3b0d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0510bf89dd4315b4d232",
+    "url": "./static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "7e5bf02546778ddfc276",
+    "url": "./static/css/main.3ddadc7e.chunk.css"
+  },
+  {
+    "revision": "0510bf89dd4315b4d232",
+    "url": "./static/js/2.7aee2e24.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7aee2e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5bf02546778ddfc276",
+    "url": "./static/js/main.baf5cc72.chunk.js"
+  },
+  {
+    "revision": "6ecb3f858d3c977b1776",
+    "url": "./static/js/runtime-main.6c52476b.js"
+  },
+  {
+    "revision": "31563ccd3b007592a504ce658a860597",
+    "url": "./static/media/logo.31563ccd.png"
+  }
+]);
